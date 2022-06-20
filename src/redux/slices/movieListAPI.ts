ddb400c084ef83584db41ed74712a8808c1e19ea
chan wai hsuen <chanwaihsuen@gmail.com>
2022-06-20 14:07:13 +0800

    feat: movie card

--- src/redux/slices/movieListAPI.ts
+++ src/redux/slices/movieListAPI.ts
@@ -1,15 +1,21 @@
+import data from '../data/sample.json';
+
 // A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
   return new Promise<{ data: number }>((resolve) => setTimeout(() => resolve({ data: amount }), 500));
 }
 
 export const fetchMovieList = async () => {
-  try {
-    const response = await fetch('../data/sample.json');
-    const movies = response.json();
-    console.log('movies', movies);
-    return response;
-  } catch (error) {
-    console.log('error:', error);
-  }
+  // console.log('data', data.entries);
+
+  return data;
+
+  // try {
+  //   const response = await fetch('../data/sample.json');
+  //   const movies = response.json();
+  //   console.log('movies', movies);
+  //   return response;
+  // } catch (error) {
+  //   console.log('error:', error);
+  // }
 };
---
+++ src/screens/home/components/MovieImage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const MovieImage = ({ url, width, height }: { url: String; width: Number; height: Number }) => {
+  return (
+    <React.Fragment>
+      {/* {images['Poster Art'].url && (
+        <CardMedia component="img" height="194" image={images['Poster Art'].url} alt="asd" />
+      )} */}
+    </React.Fragment>
+  );
+};
+
+export default MovieImage;
