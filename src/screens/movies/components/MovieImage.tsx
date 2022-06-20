c532ffab41e969d0e605759ceaacc653051dc0ea
chan wai hsuen <chanwaihsuen@gmail.com>
2022-06-20 18:43:18 +0800

    feat: added skeleton for image

--- src/screens/movies/components/MovieImage.tsx
+++ src/screens/movies/components/MovieImage.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Skeleton from '@mui/material/Skeleton';
 import CardMedia from '@mui/material/CardMedia';
 import movieposterplaceholder from 'assets/movieposterplaceholder.png';
 
-const MovieImage = ({
-  url,
-  width,
-  height,
-  title,
-  mediaHeight = 200,
-}: {
+interface IProps {
   url: String;
   width: Number;
   height: Number;
   title: String;
   mediaHeight: Number;
-}) => {
+}
+
+const MovieImage = ({ url, width, height, title, mediaHeight = 200 }: IProps) => {
+  const [hasImageLoaded, setHasImageLoaded] = useState(false);
+
   const handleImageError = (e: any) => {
     e.target.onerror = null;
     e.target.src = movieposterplaceholder;
@@ -22,12 +21,18 @@ const MovieImage = ({
 
   return (
     <React.Fragment>
+      {!hasImageLoaded && <Skeleton variant="rectangular" animation="wave" height={`${mediaHeight}px`} />}
       <CardMedia
         component="img"
         height={`${mediaHeight}`}
         image={`${url}`}
         alt={`${title}`}
+        onLoad={() => setHasImageLoaded(true)}
         onError={handleImageError}
+        loading="lazy"
+        sx={{
+          opacity: hasImageLoaded ? 1.0 : 0,
+        }}
       />
     </React.Fragment>
   );
